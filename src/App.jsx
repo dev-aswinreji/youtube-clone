f7a8e57d7703af3663f1ac551bda8353ee4111c8
Aswin Reji <aswinreji.004@gmail.com>
2024-12-09 18:39:24 +0000

    feat: Added navbar ,home ,video to create youtube

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+  <Navbar/>  
     </>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+
+
+export default function Navbar(){
+    return (
+        <>
+        <h1>Navbar</h1>
+        </>
+    )
+}
