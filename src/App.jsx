361a0f9dca9c49f101d80285f71cf903da9a95cd
Aswin Reji <aswinreji.004@gmail.com>
2024-12-09 20:06:50 +0000

    feat: Added router to home and video

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <>
   <Navbar/>  
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/video/:categoryId/:videoId' element={<video/>} />
+    </Routes>
     </>
   )
 }
