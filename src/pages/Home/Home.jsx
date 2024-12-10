18a52defbbb5e9e8e78d8ba3ce70f272fa836da9
Aswin Reji <aswinreji.004@gmail.com>
2024-12-10 17:50:45 +0000

    feat: Added icon for tech gaming new etc on sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 import './Home.css'
+import Sidebar from "../../components/Sidebar/Sidebar.jsx"
 
 function Home(){
     return (
-        <></>
+        <>
+        <Sidebar/>
+        </>
     )
 }
 
