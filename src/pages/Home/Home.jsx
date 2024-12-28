a53d441923d02588138dd34aaa83e5aa72268e8c
Aswin Reji <aswinreji.004@gmail.com>
2024-12-28 20:39:33 +0000

    feat: feed added ui and code added thumbnail

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import './Home.css'
 import Sidebar from "../../components/Sidebar/Sidebar.jsx"
+import Feed from "../../components/Feed/Feed.jsx"
 
-function Home({sidebar}){
+function Home({ sidebar }) {
     return (
         <>
-        <Sidebar sidebar={sidebar}/>
+            <Sidebar sidebar={sidebar} />
+            <div className={`container ${sidebar ? "" : "large-container"}`}>
+                <Feed/>
+            </div>
         </>
     )
 }
